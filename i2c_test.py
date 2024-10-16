21951b2e2010272366cace6babf7b5f018094971
Allan Yu <allanzyu@gmail.com>
2024-10-15 22:32:46 -0700

    Basic commands - send and receive blocks

--- i2c_test.py
+++ i2c_test.py
@@ -8,22 +8,33 @@
 from smbus import SMBus
 import time
 
-addr = 0x8 # bus address
+addr = 0x08 # bus address
 bus = SMBus(1) # indicates /dev/ic2-1
 
 numb = 1
+def StringToBytes(val):
+        retVal = []
+        for c in val:
+                retVal.append(ord(c))
+        return retVal
 
 print ("Enter 1 for ON or 0 for OFF")
 while numb == 1:
 
-	ledstate = input(">>>>   ")
+	command = input(">>>>   ")
 
-	if ledstate == "1":
+	if command == "1":
 		bus.write_byte(addr, 0x1) # switch it on
-	elif ledstate == "0":
+	elif command == "0":
 		bus.write_byte(addr, 0x0) # switch it off
-	elif ledstate =="2":
-		pin = chr(bus.read_byte(addr))
-		print ("Arduino sends: " + str(pin)) 	
+	elif command =="2":
+		test = StringToBytes("test")
+		bus.write_i2c_block_data(addr,0x01,test)
+		##print ("Arduino sends: " + str(pin))
+	elif command =="3":
+		data = bytes(bus.read_i2c_block_data(addr,0x00, 4))
+#		text = data.decode()
+#		print(text)
 	else:
 		numb = 0
+
